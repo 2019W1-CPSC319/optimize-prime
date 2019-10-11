1b683273d0efe849b9425f39913211639fcc5d41
Candice <candice@aytech.ca>
2019-10-11 14:14:47 -0700

    Updated index.js to use RootReducer explicitly

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+import RootReducer from './reducers';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+  <Provider store={createStore(RootReducer, applyMiddleware(thunk))}>
     <App />
   </Provider>,
   document.getElementById('root'),
