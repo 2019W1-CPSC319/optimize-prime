4d57d8d742bc4571a9dcff55fd01a291e5fee24b
Alice <alice.kim@concur.com>
2019-10-12 15:35:21 -0700

    create dialog to schedule interview

--- src/components/calendar/CalendarPage.jsx
+++ src/components/calendar/CalendarPage.jsx
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import EventIcon from '@material-ui/icons/Event';
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from '@material-ui/core';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
 
 const styles = {
   title: {
@@ -37,21 +54,57 @@ const styles = {
   },
   event: {
     display: 'flex',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  button: {
+    border: 'none',
+    cursor: 'pointer',
+    display: 'flex',
+    fontSize: '16px',
+    outline: 'none',
+  },
+  icon: {
+    margin: 'auto',
+  },
+  iconLabel: {
+    marginLeft: '10px',
+    marginRight: '30px',
   }
+  // galvanize: {
+  //   backgroundColor: '#280e3a',
+  // },
 };
 
 class CalendarPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      open: false,
     };
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
   }
 
   render() {
     const { classes } = this.props;
+
     return <div>
-      <h1 className={classes.title}>Calendar</h1>
+      <div className={classes.header}>
+        <h1 className={classes.title}>Calendar</h1>
+        <Button className={classes.button} onClick={this.handleOpen.bind(this)}>
+          <EventIcon className={classes.icon}></EventIcon>
+          <p className={classes.iconLabel}>Schedule Interview</p>
+        </Button>
+      </div>
       <div className={classes.calendar}>
         <div className={classes.component}>
           <p className={classes.label}>Sunday</p>
@@ -71,6 +124,31 @@ class CalendarPage extends Component {
         <div className={classes.component}><p className={classes.label}>Friday</p></div>
         <div className={classes.component}><p className={classes.label}>Saturday</p></div>
       </div>
+      <Dialog open={this.state.open} onClose={this.handleClose.bind(this)} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose.bind(this)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.handleClose.bind(this)} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>;
   }
 }
