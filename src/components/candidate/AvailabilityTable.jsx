04be3877a5fcb6dc0c2a7e0a27551eb4bd46aaa5
BenH14 <splodgemolly@gmail.com>
2019-10-15 18:07:37 -0700

    WIP availability table

---
+++ src/components/candidate/AvailabilityTable.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableRow from "@material-ui/core/TableRow"
+
+import Fab from "@material-ui/core/Fab"
+import AddIcon from "@material-ui/icons/Add"
+
+import { TableHead, Button } from '@material-ui/core';
+
+import "./AvailabilityTable.css"
+
+class AvailabilityTable extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  createRow(date, from, to) {
+      return {date, from, to};
+  }
+
+  addRow() {
+      
+  }
+
+  rows = [
+    this.createRow(1, 2, 3)
+  ]
+
+  render() {
+      return (
+            <div>
+              <Table aria-label="Availability">
+                  <TableHead>
+                      <TableRow>
+                          <TableCell>Date</TableCell>
+                          <TableCell>From</TableCell>
+                          <TableCell>To</TableCell>
+                          <TableCell></TableCell>
+                      </TableRow>
+                  </TableHead>
+                  <TableBody>
+                      {this.rows.map(row => (
+                        <TableRow>
+                          <TableCell>{row.date}</TableCell>
+                          <TableCell>{row.from}</TableCell>
+                          <TableCell>{row.to}</TableCell>
+                          <TableCell>
+                              <Button>
+                                  Remove
+                              </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+              </Table>
+              <Fab aria-label="add" size="small" className="fab-add">
+                  <AddIcon />
+              </Fab>
+            </div>
+        );
+  }
+}
+
+export default AvailabilityTable;
+
