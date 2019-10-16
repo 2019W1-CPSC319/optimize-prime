8f670689be68f6c4da88ef2fd513cbd93fc5f7e4
Martin <martinjohansen1705@gmail.com>
2019-10-15 17:55:18 -0700

    Added middleware to check if user authenticated

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,20 +1,19 @@
 const router = require('express').Router();
 const axios = require('axios');
-const graph = require("../utils/graph")
+const notAuthMiddleware = require('../utils/notAuthMiddleware');
 
-router.get('/profile', async (req, res) => {
+router.get('/profile', notAuthMiddleware, async (req, res) => {
   try {
     const response = await axios({
       url: 'https://graph.microsoft.com/v1.0/users/me',
       headers: {
         Authorization: "Bearer " + req.user.accessToken
-
       }
-    })
+    });
     
     res.send(response.data);
   } catch (err) {
-    console.log(err);
+    res.status(err.statusCode).send(err.message)
   }
 });
 
--- server/src/utils/notAuthMiddleware.js
+++ server/src/utils/notAuthMiddleware.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
 	if (!req.user || !req.isAuthenticated()) {
-		res.status(400).send("Unauthorized")
-		return
+		return res.status(400).send("Unauthorized")
 	}
 	next()
 }
