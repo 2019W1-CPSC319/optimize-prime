eb88c70bc85829b30ba68ff5f06feb842218f42e
BenH14 <splodgemolly@gmail.com>
2019-10-22 20:11:15 -0700

    Cleanup imports

--- src/components/candidate/AvailabilityTable.jsx
+++ src/components/candidate/AvailabilityTable.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import 'date-fns';
 
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableRow from "@material-ui/core/TableRow"
-
-import DateFnsUtils from '@date-io/date-fns';
+// Material UI
 import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
-
-import Select from "@material-ui/core/Select"
-import MenuItem from "@material-ui/core/MenuItem"
-
+        Table, TableBody, TableHead, TableCell, TableRow,
+        Select, MenuItem, Button
+       } from "@material-ui/core"
+import {
+        MuiPickersUtilsProvider,
+        KeyboardDatePicker,
+       } from '@material-ui/pickers';
 import Fab from "@material-ui/core/Fab"
 import AddIcon from "@material-ui/icons/Add"
 
-import { TableHead, Button } from '@material-ui/core';
+// Date Picker
+import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
 
 import "./AvailabilityTable.css"
 
@@ -69,11 +65,12 @@ const times = [
   <MenuItem value={14.75}>5:45pm</MenuItem>,
 ]
 
-class AvailabilityTable extends Component {
 
+class AvailabilityTable extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       indexctr: 1,
       rows: [this.createRow(0, new Date(), 2, 3)]
@@ -81,15 +78,16 @@ class AvailabilityTable extends Component {
   }
 
   handleAddRow = () => {
+    // Adds a row to the table
     this.setState((prevState, props) => {
       const row = this.createRow(this.state.indexctr, new Date(), 0, 0);
       const newindex = this.state.indexctr + 1
       this.setState({indexctr: newindex, rows: [...prevState.rows, row]})
-      //return {indexctr: newindex, rows: [...prevState.rows, row]};
     })
   }
 
   handleRemoveRow = (id) => {
+    // Removes the row with the specified id in the table
     const rows = this.state.rows;
     for (const row of rows) {
       if (row.id == id) {
