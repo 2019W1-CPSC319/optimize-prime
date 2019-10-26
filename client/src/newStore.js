e2149d6109d8acb13f5cef2451e73a64e200ba4e
Candice <candice@aytech.ca>
2019-10-26 15:27:30 -0700

    Added new line at the end of files

--- client/src/newStore.js
+++ client/src/newStore.js
@@ -1,9 +1,9 @@
-import { applyMiddleware, compose as reduxCompose, createStore } from "redux"
-import thunk from "redux-thunk"
-import RootReducer from "./reducers"
+import { applyMiddleware, compose as reduxCompose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import RootReducer from './reducers';
 
 // Add redux devtools to our application if available
-const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || reduxCompose
+const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || reduxCompose;
 
 /**
  * This function creates a new store instance ready for use with Redux DevTools
@@ -11,13 +11,15 @@ const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || reduxCompose
  * @returns A new store object that supports the Redux DevTools extension
  */
 
-export const newStore = () => {
-	const initialStateElement = document.getElementById("initialState")
-	let initState = {}
-	if(initialStateElement) {
-		initState = JSON.parse(initialStateElement.innerHTML || "{}")
-	}
-	return createStore(RootReducer(), initState, compose(
-		applyMiddleware(thunk)
-	))
-}
+const newStore = () => {
+  const initialStateElement = document.getElementById('initialState');
+  let initState = {};
+  if (initialStateElement) {
+    initState = JSON.parse(initialStateElement.innerHTML || '{}');
+  }
+  return createStore(RootReducer(), initState, compose(
+    applyMiddleware(thunk),
+  ));
+};
+
+export default newStore;
