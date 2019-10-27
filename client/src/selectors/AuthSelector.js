1798af3e4f507ba95b240c1dd34da66024aa400f
Candice <candice@aytech.ca>
2019-10-26 17:22:14 -0700

    Added index.js to contain all selectors

--- client/src/selectors/AuthSelector.js
+++ client/src/selectors/AuthSelector.js
@@ -1,4 +1,3 @@
-// helper functions to keep the data types in the components consistent
 export const getState = (globalState) => globalState.user;
 export const getUserProfile = (state) => getState(state).profile || null;
 export const isLoading = (state) => !!getState(state).loading;
---
+++ client/src/selectors/index.js
@@ -0,0 +1,3 @@
+// helper functions to keep the data types in the components consistent
+
+export * from './AuthSelector';
