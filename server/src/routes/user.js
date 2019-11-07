1bce12dbb649e7620927f2d5fcffab728cca8d6d
Alice <alice.kim@concur.com>
2019-11-06 22:48:38 -0800

    add test

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -51,53 +51,31 @@ router.post('/sendemail', notAuthMiddleware, async (req, res) => {
 });
 
 router.get('/findMeetingTimes', notAuthMiddleware, async (req, res) => {
+  // attendee: { type: "(required|optional|resource)", emailAddress: { "@odata.type": "@." } }
+
   try {
     const response = await axios({
       url: 'https://graph.microsoft.com/v1.0/users/me/findMeetingTimes',
       headers: {
         Authorization: `Bearer ${req.user.accessToken}`,
       },
+      data: {
+        attendees: req.body.attendees,
+        isOrganizerOptional: true,
+        locationConstraint: {
+          isRequired: false,
+          locations: [],
+          suggestLocation: true,
+        },
+        meetingDuration: req.body.meetingDuration,
+        timeConstraint: ,
+
+      },
     });
 
     res.send(response.data);
   } catch (err) {
     res.status(err.response.status).send(err.message);
-  }
-});
-
-router.post('/sendemail', notAuthMiddleware, async (req, res) => {
-  try {
-    const response = await axios({
-      method: 'post',
-      url: 'https://graph.microsoft.com/v1.0/me/sendMail',
-      headers: {
-        Authorization: `Bearer ${req.user.accessToken}`,
-      },
-      data: {
-        message: {
-          subject: req.body.subject,
-          body: {
-            contentType: 'text',
-            content: req.body.body,
-          },
-          toRecipients: [
-            {
-              emailAddress: {
-                address: 'candicekhpang@hotmail.com.hk',
-              },
-            },
-            {
-              emailAddress: {
-                address: 'martinjohansen1705@gmail.com',
-              },
-            },
-          ],
-        },
-      },
-    });
-    res.send(response.data);
-  } catch (error) {
-    console.log(error);
   }
 });
 
