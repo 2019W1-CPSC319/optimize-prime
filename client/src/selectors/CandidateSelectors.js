2a2ff1b911bed1ee22882ca7915ffb20175807ca
Alice <alice.kim@concur.com>
2019-11-11 15:56:20 -0800

    remove unused selectors

--- client/src/selectors/CandidateSelectors.js
+++
@@ -1,5 +0,0 @@
-export const getState = (globalState) => globalState.candidates || {};
-export const getCandidates = (state) => getState(state).candidates || [];
-export const getCandidateById = (state, uuid) => (
-  getCandidates(state).find((candidate) => candidate.uuid.toString() === uuid) || null
-);
--- client/src/selectors/InterviewerSelectors.js
+++
@@ -1,2 +0,0 @@
-export const getState = (globalState) => globalState.interviewers || {};
-export const getInterviewers = (state) => getState(state).interviewers || [];
