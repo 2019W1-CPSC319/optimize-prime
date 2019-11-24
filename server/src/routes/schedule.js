9728b68872854bfba995c4c6b3f667386de569fc
Candice <candice@aytech.ca>
2019-11-24 00:30:49 -0800

    Added preferred time zone in find meeting times

--- server/src/routes/schedule.js
+++ server/src/routes/schedule.js
@@ -373,6 +373,7 @@ router.post('/meeting', notAuthMiddleware, async (req, res) => {
               url: 'https://graph.microsoft.com/v1.0/me/findmeetingtimes',
               headers: {
                 Authorization: `Bearer ${req.user.accessToken}`,
+                Prefer: `outlook.timezone="${timeZone}"`,
               },
               data,
             });
