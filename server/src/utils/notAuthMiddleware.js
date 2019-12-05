da6fddf3ba99a5a3a9a85c3ddc580edc276ce0f3
Candice <candice@aytech.ca>
2019-12-04 20:49:24 -0800

    Corrected error message from 401 to 403 for both front and back end

--- server/src/utils/notAuthMiddleware.js
+++ server/src/utils/notAuthMiddleware.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   if (!req.user || !req.isAuthenticated()) {
-    return res.status(401).send('Unauthorized');
+    return res.status(403).send('Forbidden');
   }
   next();
 };
